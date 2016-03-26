568b5ff4517be60fb24b502070ac86e599464af3
Motishjain <motishj@ymail.com>
2016-03-26 15:05:40 +0530

    UI improvements

--- app/src/main/java/com/example/admin/util/JsonConversionTest.java
+++ app/src/main/java/com/example/admin/util/JsonConversionTest.java
@@ -1,5 +1,7 @@
 package com.example.admin.util;
 
+import com.example.admin.constants.AppConstants;
+import com.example.admin.webservice.request_objects.OutletRequest;
 import com.example.admin.webservice.request_objects.RewardSubmitRequest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -31,5 +33,17 @@ public class JsonConversionTest {
         GsonBuilder builder = new GsonBuilder();
         Gson gson  = builder.create();
         System.out.println(gson.toJson(request));
+
+
+        OutletRequest outletRequest = new OutletRequest();
+        outletRequest.setOutletName("bhf");
+        outletRequest.setAliasName("dnjd");
+        outletRequest.setAddrLine1("bshs");
+        outletRequest.setOutletType("RET");
+        outletRequest.setAddrLine2("dndjd");
+        outletRequest.setPinCode("777777");
+        outletRequest.setEmail("motish@gh.com");
+        outletRequest.setCellNumber("6464646464");
+        System.out.println(gson.toJson(outletRequest));
     }
 }
