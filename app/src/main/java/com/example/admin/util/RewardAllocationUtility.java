a6d783a9cfca09d1802788106a15be574f68b2ff
Motishjain <motishj@ymail.com>
2016-04-01 19:59:13 +0530

    UI improvements

--- app/src/main/java/com/example/admin/util/RewardAllocationUtility.java
+++ app/src/main/java/com/example/admin/util/RewardAllocationUtility.java
@@ -17,135 +17,117 @@ import java.util.Random;
 
 public class RewardAllocationUtility {
 
-    public static int allocateReward(String userPhoneNumber, int billAmount,Dao<SelectedReward, Integer> selectedRewardDao, Dao<User, Integer> userDao) {
+    public static SelectedReward allocateReward(String userPhoneNumber, int billAmount, Dao<SelectedReward, Integer> selectedRewardDao, Dao<User, Integer> userDao) {
 
-        double bronzeRatio=0,silverRatio=0,goldRatio=0;
+        double bronzeRatio = 0, silverRatio = 0, goldRatio = 0;
         int categoryAllocated = 0, carryForwardAmount = 0, targetAmount;
         try {
             QueryBuilder<User, Integer> userQueryBuilder = userDao.queryBuilder();
             userQueryBuilder.where().eq("phoneNumber", userPhoneNumber);
             User currentUser = userQueryBuilder.queryForFirst();
             carryForwardAmount = currentUser.getCarryForwardAmount();
-        }
-        catch(SQLException e) {
+        } catch (SQLException e) {
             //TODO handle error
-        }
-        catch(Exception e){
+        } catch (Exception e) {
 
         }
 
         targetAmount = billAmount + carryForwardAmount;
 
-        if(targetAmount>=500 && targetAmount<1000){
-            bronzeRatio = 15.0 + (targetAmount-500)/20.0;
-        }
-        else if(targetAmount>=1000 && targetAmount<3000){
+        if (targetAmount >= 500 && targetAmount < 1000) {
+            bronzeRatio = 15.0 + (targetAmount - 500) / 20.0;
+        } else if (targetAmount >= 1000 && targetAmount < 3000) {
             //Chances are in order Bronze (40% or more), Silver (5% or less), Gold (0%)
 
             //40% + (max 40% of remaining depending on target amount)
             //ex: if target amount = 2000, bR = 40 + 20% of 60 = 52%
-            bronzeRatio = 40.0 + (targetAmount-1000)* 3.0/250.0;
+            bronzeRatio = 40.0 + (targetAmount - 1000) * 3.0 / 250.0;
+            silverRatio = (float) (targetAmount - 1000) / 50.0 * (100.0 - bronzeRatio) / 100.0;
 
-
-            silverRatio = (float)(targetAmount-1000)/50.0 * (100.0-bronzeRatio)/100.0;
-
-        }
-        else if(targetAmount>=3000 && targetAmount<6000){
+        } else if (targetAmount >= 3000 && targetAmount < 6000) {
             //Chances are in order Silver (40% or more), Bronze (7% or less), Gold (6% or less)
             silverRatio = 40;
             bronzeRatio = 24;
             goldRatio = 7;
-            silverRatio += ((100.0-(silverRatio+bronzeRatio+goldRatio))*(float)(targetAmount-3000)/7500);
-            goldRatio += ((100.0-(silverRatio+bronzeRatio+goldRatio))*(float)(targetAmount-3000)/7500);
-            
-	    //bronzeRatio += ((100.0-(silverRatio+bronzeRatio+goldRatio))*(float)(targetAmount-3000)/7500);
-            
-	    bronzeRatio = (100.0 - (silverRatio + goldRatio));
-        }
-        else {
+            silverRatio += ((100.0 - (silverRatio + bronzeRatio + goldRatio)) * (float) (targetAmount - 3000) / 7500);
+            goldRatio += ((100.0 - (silverRatio + bronzeRatio + goldRatio)) * (float) (targetAmount - 3000) / 7500);
+
+            bronzeRatio = (100.0 - (silverRatio + goldRatio));
+        } else {
             //Chances are in order Gold (40% or more), Silver (20% or less), Bronze (12% or less)
             goldRatio = 40;
             silverRatio = 24;
             bronzeRatio = 14;
-            for (int i=targetAmount; i>=7000; i-=1000){
-                goldRatio += (100-(silverRatio+bronzeRatio+goldRatio))*0.2;
-                silverRatio += (100-(silverRatio+bronzeRatio+goldRatio))*0.2;
-                bronzeRatio += (100-(silverRatio+bronzeRatio+goldRatio))*0.2;
+            for (int i = targetAmount; i >= 7000; i -= 1000) {
+                goldRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2;
+                silverRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2;
+                bronzeRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2;
             }
-            int i = targetAmount%1000;
-            goldRatio += (100-(silverRatio+bronzeRatio+goldRatio))*0.2*i/1000;
-            silverRatio += (100-(silverRatio+bronzeRatio+goldRatio))*0.2*i/1000;
-            bronzeRatio += (100-(silverRatio+bronzeRatio+goldRatio))*0.2*i/1000;
+            int i = targetAmount % 1000;
+            goldRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2 * i / 1000;
+            silverRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2 * i / 1000;
+            bronzeRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2 * i / 1000;
 
-	    silverRatio = (100.0-(bronzeRatio+goldRatio));
+            silverRatio = (100.0 - (bronzeRatio + goldRatio));
         }
 
         Random randomGenerator = new SecureRandom();
 
         int randomNumber = randomGenerator.nextInt(100);
 
-        if(randomNumber<bronzeRatio){
+        if (randomNumber < bronzeRatio) {
             //Bronze allocated
             categoryAllocated = 1;
-        }
-        else if(randomNumber<(bronzeRatio+silverRatio)){
+        } else if (randomNumber < (bronzeRatio + silverRatio)) {
             //Silver allocated
             categoryAllocated = 2;
-        }
-        else if(randomNumber<(bronzeRatio+silverRatio+goldRatio)){
+        } else if (randomNumber < (bronzeRatio + silverRatio + goldRatio)) {
             //Gold allocated
             categoryAllocated = 3;
         }
         System.out.println(categoryAllocated);
-        // if(categoryAllocated>0 && selectedRewardDao!=null)
-        // {
-        //     return getReward(selectedRewardDao,categoryAllocated);
-        // }
-        //else {
-        //    return null;
-        // }
-        return categoryAllocated;
+        if (categoryAllocated > 0 && selectedRewardDao != null) {
+            return getReward(selectedRewardDao, categoryAllocated);
+        } else {
+            return null;
+        }
     }
 
     public static void main(String args[]) {
-        allocateReward("123",2500,null,null);
-        try{
-            int i=0;
+        allocateReward("123", 2500, null, null);
+        try {
+            int i = 0;
 
             PrintWriter writer = new PrintWriter("C:/Users/bgurbuxsinghani/Documents/stuff/Algo/algoanalysis.txt", "UTF-8");
-            for(i=1;i<100;i++)
-            {
+            for (i = 1; i < 100; i++) {
                 Random r = new Random();
                 int Low = 300;
                 int High = 10000;
-                int Result = r.nextInt(High-Low) + Low;
+                int Result = r.nextInt(High - Low) + Low;
                 writer.append(i + "," + Result + "," + (allocateReward("123", Result, null, null)));
                 writer.println();
 
             }
             writer.close();
+        } catch (Exception e) {
         }
-
-        catch(Exception e){}
 
 
     }
 
     private static SelectedReward getReward(Dao<SelectedReward, Integer> selectedRewardDao, int categoryAllocated) {
-        QueryBuilder<SelectedReward,Integer> selectedRewardQueryBuilder = selectedRewardDao.queryBuilder();
+        QueryBuilder<SelectedReward, Integer> selectedRewardQueryBuilder = selectedRewardDao.queryBuilder();
         try {
-            selectedRewardQueryBuilder.where().eq("rewardCategory",categoryAllocated);
+            selectedRewardQueryBuilder.where().eq("rewardCategory", categoryAllocated);
             List<SelectedReward> possibleRewards = selectedRewardQueryBuilder.query();
-            if(possibleRewards.size()>0){
+            if (possibleRewards.size() > 0) {
                 Random randomGenerator = new SecureRandom();
                 int randomRewardIndex = randomGenerator.nextInt(possibleRewards.size());
                 return possibleRewards.get(randomRewardIndex);
+            } else if (categoryAllocated > 1) {
+                return getReward(selectedRewardDao, categoryAllocated - 1);
             }
-            else if (categoryAllocated>1){
-                return getReward(selectedRewardDao,categoryAllocated-1);
-            }
-        }
-        catch(SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
