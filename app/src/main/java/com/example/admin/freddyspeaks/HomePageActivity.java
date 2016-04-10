5e7945822591586a3c6121d1931e0f20410e3257
Motishjain <motishj@ymail.com>
2016-04-10 17:02:25 +0530

    UI improvements

--- app/src/main/java/com/example/admin/freddyspeaks/HomePageActivity.java
+++ app/src/main/java/com/example/admin/freddyspeaks/HomePageActivity.java
@@ -2,15 +2,16 @@ package com.example.admin.freddyspeaks;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-import com.example.admin.webservice.request_objects.FeedbackRequest;
+import com.example.admin.tasks.AsyncImageSetterTask;
 
 public class HomePageActivity extends BaseActivity {
 
+    ImageView backgroundRatingImage;
     Button addRating;
 
     @Override
@@ -19,6 +20,10 @@ public class HomePageActivity extends BaseActivity {
         setContentView(R.layout.activity_home_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        backgroundRatingImage = (ImageView) findViewById(R.id.backgroundRatingImage);
+        AsyncImageSetterTask asyncImageSetterTask = new AsyncImageSetterTask(this,backgroundRatingImage);
+        asyncImageSetterTask.execute(R.drawable.bags);
 
         addRating = (Button) findViewById(R.id.add_rating);
 
