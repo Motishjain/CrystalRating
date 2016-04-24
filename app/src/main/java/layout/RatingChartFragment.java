5d8e0b70c2dd054807188da569bc8a7ee538bfe1
Motishjain <motishj@ymail.com>
2016-04-25 00:44:01 +0530

    general fixes

--- app/src/main/java/layout/RatingChartFragment.java
+++ app/src/main/java/layout/RatingChartFragment.java
@@ -169,24 +169,6 @@ public class RatingChartFragment extends Fragment {
         return ratingChartFragment;
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
     public void refreshPieChart() {
         List<Entry> entries = new ArrayList<>();
         List<String> labels = new ArrayList<>();
