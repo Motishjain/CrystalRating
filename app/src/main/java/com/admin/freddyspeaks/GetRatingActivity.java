4874edc86a728859b8a08116144fb6b2e11a00bb
Motishjain <motishj@ymail.com>
2016-05-04 10:15:12 +0530

    fixes

--- app/src/main/java/com/admin/freddyspeaks/GetRatingActivity.java
+++ app/src/main/java/com/admin/freddyspeaks/GetRatingActivity.java
@@ -202,7 +202,7 @@ public class GetRatingActivity extends BaseActivity implements RatingCardFragmen
                 }
             };
             Handler intervalHandler = new Handler();
-            intervalHandler.postDelayed(getNextRatingTask,200);
+            intervalHandler.postDelayed(getNextRatingTask,400);
 
         }
     }
