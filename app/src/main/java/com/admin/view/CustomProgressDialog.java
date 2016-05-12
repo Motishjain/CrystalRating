069db0271020c486afcd3670e15471fa4fda1527
Bhupender <bhupenderg99@gmail.com>
2016-05-12 22:23:52 +0530

    Screen changes, validations, fonts, colors modified.

--- app/src/main/java/com/admin/view/CustomProgressDialog.java
+++ app/src/main/java/com/admin/view/CustomProgressDialog.java
@@ -17,7 +17,7 @@ public class CustomProgressDialog extends ProgressDialog {
 
     public static ProgressDialog createCustomProgressDialog(Context context) {
 
-        CustomProgressDialog dialog = new CustomProgressDialog(context);
+        CustomProgressDialog dialog = new CustomProgressDialog(context,R.style.MyTheme);
         dialog.setIndeterminate(true);
         dialog.setCancelable(false);
         return dialog;
