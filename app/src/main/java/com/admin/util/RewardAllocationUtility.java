1d84538320bf3db2dd5f8e2af05241335dd86acd
Motishjain <motishj@ymail.com>
2016-05-26 23:02:46 +0530

    random allocation removed - rewards will be allocated directly based on cost

--- app/src/main/java/com/admin/util/RewardAllocationUtility.java
+++ app/src/main/java/com/admin/util/RewardAllocationUtility.java
@@ -11,6 +11,7 @@ import com.j256.ormlite.stmt.QueryBuilder;
 import java.io.PrintWriter;
 import java.security.SecureRandom;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,40 +23,38 @@ import java.util.Random;
 
 public class RewardAllocationUtility {
 
-    static int carryForwardAmount = 0;
-    public static Map<Integer,String> categoryMapping = new HashMap<>();
-    static {
-        categoryMapping.put(1, AppConstants.BRONZE_CD);
-        categoryMapping.put(2, AppConstants.SILVER_CD);
-        categoryMapping.put(3, AppConstants.GOLD_CD);
-    }
+    public static SelectedReward allocateReward(int billAmount, Dao<SelectedReward, Integer> selectedRewardDao) {
+        double rewardAmountMean = (1.5 * billAmount) / 100;
+        double rewardAmountLowerBound = (1 * billAmount) / 100;
+        double rewardAmountUpperBound = (2 * billAmount) / 100;
+        double minDiff = 9999;
+        int selectedRewardIndex = -1;
+        List<Integer> selectedRewardIndexList = new ArrayList<>();
 
-    public static final int REWARD_AMOUNT_THRESHOLD_1 = 500;
-
-    public static final int REWARD_AMOUNT_THRESHOLD_2 = 3000;
-
-    public static final int REWARD_AMOUNT_THRESHOLD_3 = 6000;
-
-    public static SelectedReward allocateReward(String userPhoneNumber, int billAmount, Dao<SelectedReward, Integer> selectedRewardDao, Dao<User, Integer> userDao) {
-
-        int categoryAllocated = 0, targetAmount;
+        QueryBuilder<SelectedReward, Integer> selectedRewardQueryBuilder = selectedRewardDao.queryBuilder();
+        selectedRewardQueryBuilder.orderBy("rewardCost",true);
         try {
-            QueryBuilder<User, Integer> userQueryBuilder = userDao.queryBuilder();
-            userQueryBuilder.where().eq("phoneNumber", userPhoneNumber);
-            User currentUser = userQueryBuilder.queryForFirst();
-            carryForwardAmount = currentUser.getCarryForwardAmount();
+            List<SelectedReward> possibleRewards = selectedRewardQueryBuilder.query();
+            for(int i = 0;i < possibleRewards.size();i++) {
+                SelectedReward possibleReward = possibleRewards.get(i);
+                int cost = Integer.parseInt(possibleReward.getRewardCost());
+                if(Math.abs(cost-rewardAmountMean)<minDiff) {
+                    selectedRewardIndex = i;
+                    minDiff = Math.abs(cost-rewardAmountMean);
+                }
+                if(rewardAmountLowerBound<=cost && cost<=rewardAmountUpperBound) {
+                    selectedRewardIndexList.add(i);
+                }
+            }
+            if(selectedRewardIndexList.size()>=0) {
+                Random randomGenerator = new SecureRandom();
+                selectedRewardIndex = selectedRewardIndexList.get(randomGenerator.nextInt(selectedRewardIndexList.size()));
+            }
+            return possibleRewards.get(selectedRewardIndex);
         } catch (SQLException e) {
-            Log.e("Reward Allocation","Unable to fetch user");
+            e.printStackTrace();
         }
-
-        targetAmount = billAmount + carryForwardAmount;
-        categoryAllocated = allocateCategory(targetAmount);
-
-        if (categoryAllocated > 0 && selectedRewardDao != null) {
-            return getReward(selectedRewardDao, categoryAllocated);
-        } else {
-            return null;
-        }
+        return null;
     }
 
     public static void main(String args[]) {
@@ -67,108 +66,15 @@ public class RewardAllocationUtility {
                 int low = 200;
                 int high = 10000;
                 int result = r.nextInt(high - low) + low;
-                writer.append(i + "," + result + "," + (allocateCategory(result)));
+                writer.append(i + "," + result + "," + (allocateReward(result, null)));
                 writer.println();
-
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            if(writer!=null) {
+        } finally {
+            if (writer != null) {
                 writer.close();
             }
         }
-
-
-    }
-
-    private static int allocateCategory(int targetAmount) {//target amt + carry forward amt
-
-
-        int categoryAllocated = 0;
-        double bronzeRatio = 0, silverRatio = 0, goldRatio = 0;
-        if (targetAmount >= REWARD_AMOUNT_THRESHOLD_1 && targetAmount < REWARD_AMOUNT_THRESHOLD_2) {
-            //Chances are in order Bronze (50% and more), Silver (5% or less), Gold (0%)
-
-            //Bronze is 50% + (max 50% of remaining depending on target amount)
-            //ex: if target amount = 2000, bR = 50 + 30% of 50 = 65%
-            bronzeRatio = 50.0 + (targetAmount - 500) * 1.0 / 100.0;
-            silverRatio = (float) (targetAmount - 500) / 100.0 * (100.0 - bronzeRatio) / 100.0;
-
-        } else if (targetAmount >= REWARD_AMOUNT_THRESHOLD_2 && targetAmount < REWARD_AMOUNT_THRESHOLD_3) {
-            //Chances are in order Silver (40% or more), Bronze (7% or less), Gold (6% or less)
-            silverRatio = 40;
-            bronzeRatio = 24;
-            goldRatio = 7;
-            silverRatio += ((100.0 - (silverRatio + bronzeRatio + goldRatio)) * (float) (targetAmount - REWARD_AMOUNT_THRESHOLD_2) / 7500);
-            goldRatio += ((100.0 - (silverRatio + bronzeRatio + goldRatio)) * (float) (targetAmount - REWARD_AMOUNT_THRESHOLD_2) / 7500);
-
-            bronzeRatio = (100.0 - (silverRatio + goldRatio));
-        } else if (targetAmount >= REWARD_AMOUNT_THRESHOLD_3) {
-            //Chances are in order Gold (40% or more), Silver (20% or less), Bronze (12% or less)
-            goldRatio = 40;
-            silverRatio = 24;
-            bronzeRatio = 14;
-            for (int i = targetAmount; i >= 7000; i -= 1000) {
-                goldRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2;
-                silverRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2;
-                bronzeRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2;
-            }
-            int i = targetAmount % 1000;
-            goldRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2 * i / 1000;
-            silverRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2 * i / 1000;
-            bronzeRatio += (100 - (silverRatio + bronzeRatio + goldRatio)) * 0.2 * i / 1000;
-
-            silverRatio = (100.0 - (bronzeRatio + goldRatio));
-        }
-
-        Random randomGenerator = new SecureRandom();
-
-        int randomNumber = randomGenerator.nextInt(100);
-
-        if (randomNumber < bronzeRatio) {
-            //Bronze allocated
-            categoryAllocated = 1;
-            if(targetAmount < REWARD_AMOUNT_THRESHOLD_1)
-                carryForwardAmount = 0;
-            else
-                carryForwardAmount = targetAmount - REWARD_AMOUNT_THRESHOLD_1;
-
-        } else if (randomNumber < (bronzeRatio + silverRatio)) {
-            //Silver allocated
-            categoryAllocated = 2;
-            if(targetAmount < REWARD_AMOUNT_THRESHOLD_2)
-                carryForwardAmount = 0;
-            else
-                carryForwardAmount = targetAmount - REWARD_AMOUNT_THRESHOLD_2;
-
-        } else if (randomNumber < (bronzeRatio + silverRatio + goldRatio)) {
-            //Gold allocated
-            categoryAllocated = 3;
-            if(targetAmount < REWARD_AMOUNT_THRESHOLD_3)
-                carryForwardAmount = 0;
-            else
-                carryForwardAmount = targetAmount - REWARD_AMOUNT_THRESHOLD_3;
-        }
-        return categoryAllocated;
-    }
-
-    private static SelectedReward getReward(Dao<SelectedReward, Integer> selectedRewardDao, int categoryAllocated) {
-        QueryBuilder<SelectedReward, Integer> selectedRewardQueryBuilder = selectedRewardDao.queryBuilder();
-        try {
-            selectedRewardQueryBuilder.where().eq("rewardCategory", categoryMapping.get(categoryAllocated));
-            List<SelectedReward> possibleRewards = selectedRewardQueryBuilder.query();
-            if (possibleRewards.size() > 0) {
-                Random randomGenerator = new SecureRandom();
-                int randomRewardIndex = randomGenerator.nextInt(possibleRewards.size());
-                return possibleRewards.get(randomRewardIndex);
-            } else if (categoryAllocated > 1) {
-                return getReward(selectedRewardDao, categoryAllocated - 1);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
