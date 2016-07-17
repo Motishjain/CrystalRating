0ce7c01e1543110805c994dc0dcbc6b4284e6c03
Motishjain <motishj@ymail.com>
2016-07-17 10:01:32 +0530

    auto redirect to home page after 3 minutes

--- app/src/main/java/com/admin/crystalrating/ThankYouActivity.java
+++ app/src/main/java/com/admin/crystalrating/ThankYouActivity.java
@@ -1,9 +1,11 @@
 package com.admin.crystalrating;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -40,6 +42,22 @@ public class ThankYouActivity extends AppCompatActivity {
         Intent homePage = new Intent(ThankYouActivity.this, HomePageActivity.class);
         homePage.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(homePage);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                Intent homePage = new Intent(ThankYouActivity.this, HomePageActivity.class);
+                homePage.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homePage);
+            }
+        }, 3*60*1000);
     }
 
     @Override
